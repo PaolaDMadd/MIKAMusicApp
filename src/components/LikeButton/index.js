ad7e8125a664aff75398bbede129f87f7a55bca4
PaolaDM <orchidea.preziosa@gmail.com>
2020-11-26 10:40:08 +0000

    added fontawesome like icon

--- src/components/LikeButton/index.js
+++ src/components/LikeButton/index.js
@@ -13,7 +13,8 @@ class LikeButton extends Component {
         return (
             // needs a thumb up instead of string
             <>
-                <button onClick={this.handleLike} style={{background: this.state.liked ? 'green' : 'red'}}>👍</button>
+                {/* <button onClick={this.handleLike} style={{background: this.state.liked ? 'green' : 'red'}}>👍</button> */}
+                <button onClick={this.handleLike} style={{color: this.state.liked ? 'orange' : 'gray'}}><i class="far fa-thumbs-up"></i></button> 
             </>
         );
     };
